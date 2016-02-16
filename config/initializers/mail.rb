7daf85d0b15a7faf96d9d0dbc86f3b854954173b
Karan Jain <kjain87@gmail.com>
2016-02-15 22:35:36 -0800

    Using Mandril APIKEY instead of API_KEY

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.smtp_settings = {
   port: '587',
   address: 'smtp.mandrillapp.com',
   user_name: ENV.fetch('MANDRILL_USERNAME'),
-  password: ENV.fetch('MANDRILL_API_KEY'),
+  password: ENV.fetch('MANDRILL_APIKEY'),
   domain: 'heroku.com',
   authentication: :plain
 }
