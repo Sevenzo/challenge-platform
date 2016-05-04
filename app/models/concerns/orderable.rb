0615a4b023b8af3a0f18182292cdd2430bc23143
Stedman <stedmanhood@gmail.com>
2016-05-04 16:41:09 -0700

    Add 'latest' scope to orderable.rb to override default_scope and order items by created_at: :desc

--- app/models/concerns/orderable.rb
+++ app/models/concerns/orderable.rb
@@ -2,9 +2,14 @@ module Orderable
   extend ActiveSupport::Concern
 
   included do
-    default_scope { 
-      order(featured: :desc, comments_count: :desc, created_at: :desc, id: :desc) 
+    scope :latest, -> {
+      # Escape the default_scope
+      unscoped.order(created_at: :desc)
+    }
+
+    default_scope {
+      order(featured: :desc, comments_count: :desc, created_at: :desc, id: :desc)
     }
   end
-  
+
 end
