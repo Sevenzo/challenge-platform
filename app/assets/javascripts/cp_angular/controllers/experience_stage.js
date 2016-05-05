02d5e6e9d44661e6e32bd8c38f90b26d44717157
Stedman <stedmanhood@gmail.com>
2016-05-05 08:12:27 -0700

    Test: set .theme_:id_experiences in the angular controller

--- app/assets/javascripts/cp_angular/controllers/experience_stage.js
+++ app/assets/javascripts/cp_angular/controllers/experience_stage.js
@@ -5,6 +5,13 @@ app.controller('ExperienceStageCtrl', function($scope, $document, $timeout) {
   // For the front end, update the view and scroll to the right element
   $timeout(function(){
     if ($scope.show_all) {
+      // Use the hash to generate the scope variable that we'd like to set to `true.
+      // Starting with `hash`, of the form `#theme-1` => construct scope variable name `theme_1_experiences`
+      var themeIdExperiences = (hash.split('#')[1]+'_exJHperiences').replace(/-/,'_');
+
+      $scope[themeIdExperiences] = true;
+      console.log('========================', themeIdExperiences, $scope[themeIdExperiences]);
+
       var target = angular.element(document.getElementById(hash.substring(1)));
       $document.scrollToElement(target, 60, 2000);
     }
