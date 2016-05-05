6d7a9261e43f04c6b9c13c394e34213f025c8335
Stedman <stedmanhood@gmail.com>
2016-05-05 10:01:39 -0700

    Move theme_:id_experience's ng-init logic to the experience_stage angular controller.

--- app/assets/javascripts/cp_angular/controllers/experience_stage.js
+++ app/assets/javascripts/cp_angular/controllers/experience_stage.js
@@ -2,6 +2,15 @@ app.controller('ExperienceStageCtrl', function($scope, $document, $timeout) {
   var hash = window.location.hash;
   $scope.show_all = hash != '';
 
+  // On page load, we only want to set theme_:id_experiences to true on initial page load if that theme is in the url hash.
+  $scope.initialize = function(themeIdExperiences) {
+    // Use the hash to generate the scope variable that we'd like to set to `true`.
+    // For example, with `hash`, of the form `#theme-1` => construct scope variable name `theme_1_experiences`
+    var themeIdExperienceToShow = (hash.split('#')[1]+'_experiences').replace(/-/,'_');
+
+    $scope[themeIdExperiences] = (themeIdExperiences === themeIdExperienceToShow) ? true : false;
+  };
+
   // For the front end, update the view and scroll to the right element
   $timeout(function(){
     if ($scope.show_all) {
