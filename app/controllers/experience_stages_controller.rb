5a8bdcabafc9561df28c306121668cdab77cd499
Stedman <stedmanhood@gmail.com>
2016-05-05 11:24:30 -0700

    ExperienceStages controller: add order_by variable; Link to Latest in ExperienceStages#show

--- app/controllers/experience_stages_controller.rb
+++ app/controllers/experience_stages_controller.rb
@@ -1,11 +1,11 @@
 class ExperienceStagesController < ApplicationController
 
   def show
-    # raise params.inspect
     @challenge = Challenge.find(params[:challenge_id])
     @experience_stage = @challenge.experience_stage
     @themes = @experience_stage.themes
     @featured_experiences = @experience_stage.experiences.where(featured: true)
+    @order_by = params[:order_by]
   end
 
 end
