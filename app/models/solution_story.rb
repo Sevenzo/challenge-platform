6d69548fbe1ef15b7a92a62e7fb646fe04d8ed8a
Stedman <stedmanhood@gmail.com>
2016-05-08 20:45:39 -0700

    Add order_by scope to the SolutionStory model

--- app/models/solution_story.rb
+++ app/models/solution_story.rb
@@ -2,6 +2,12 @@ class SolutionStory < ActiveRecord::Base
   include Embeddable
   include URLNormalizer
   default_scope { order(created_at: :asc) }
+  scope :order_by, lambda{ |ordering_criteria|
+    if ordering_criteria.present?
+      # Escape the default_scope's ordering, and order by the field provided
+      unscope(:order).order(ordering_criteria)
+    end
+  }
 
   belongs_to :solution_stage
   has_one :solution
