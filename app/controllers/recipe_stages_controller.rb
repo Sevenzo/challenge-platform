3629af69078de7de18d7014f2c60dfd0524bf5ce
Karan Jain <kjain87@gmail.com>
2016-05-10 19:56:31 -0700

    minor cleanup and specification

--- app/controllers/recipe_stages_controller.rb
+++ app/controllers/recipe_stages_controller.rb
@@ -5,15 +5,7 @@ class RecipeStagesController < ApplicationController
     @recipe_stage = @challenge.recipe_stage
     @cookbooks = @recipe_stage.cookbooks
     @featured_recipes = @recipe_stage.recipes.where(featured: true)
-    @ordering_criteria = valid_params
+    @ordering_criteria = 'published_at DESC' if params[:order_by] == 'latest'
   end
 
-  private
-
-  def valid_params
-    # Alternative to the default_scope's ordering.
-    if params[:order_by] == 'latest'
-      'created_at DESC'
-    end
-  end
 end
