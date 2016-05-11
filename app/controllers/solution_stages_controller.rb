b60a5182112f77709defbeb20e91f5ae14f53ded
Karan Jain <kjain87@gmail.com>
2016-05-10 20:03:48 -0700

    Cleaning up / reusing css styles

--- app/controllers/solution_stages_controller.rb
+++ app/controllers/solution_stages_controller.rb
@@ -4,15 +4,7 @@ class SolutionStagesController < ApplicationController
     @challenge = Challenge.find(params[:challenge_id])
     @solution_stage = @challenge.solution_stage
     @solution_stories = @solution_stage.solution_stories
-    @ordering_criteria = valid_params
+    @ordering_criteria = 'created_at DESC' if params[:order_by] == 'latest'
   end
 
-  private
-
-  def valid_params
-    # Alternative to the default_scope's ordering.
-    if params[:order_by] == 'latest'
-      'created_at DESC'
-    end
-  end
 end
