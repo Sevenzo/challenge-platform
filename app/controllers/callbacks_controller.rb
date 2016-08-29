4a3bd9e8070c1d8e168cb42464b4695bc57be515
Stedman <stedmanhood@gmail.com>
2016-08-29 11:45:48 -0700

    Add flash message after a user signs in or signs up with Facebook.

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -4,6 +4,7 @@ class CallbacksController < Devise::OmniauthCallbacksController
     # See: http://stackoverflow.com/questions/7999907/passing-random-url-params-to-omniauth
     @user = User.from_omniauth(request.env["omniauth.auth"], request.env["omniauth.params"])
 
+    flash[:success] = "You've signed in with Facebook!"
     sign_in_and_redirect @user
   end
 end
