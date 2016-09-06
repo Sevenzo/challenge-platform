165c17338dd91e60a0ddbbae36d2334c98171f34
Stedman <stedmanhood@gmail.com>
2016-09-05 23:48:59 -0700

    Complete the omniauth callback outline.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -9,24 +9,27 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # Check for an existing user with this email
     @user = User.find_by_email(auth.info.email)
 
-    # If this email is already associated with a user
-    if @user
-      # Is that user registered with the same provider?
-        # log them in.
-      # Else
-        # "associate" their acc't with facebook
-        # Is that user logged in already?
-          # Redirect to the homepage with flash message "You've successfully connected your Facebook account"
-        # else
-          # sign_in_and_redirect @user with flash message "You've successfully connected your Facebook account""
-      # End
+    if user_signed_in?
+      # If signed in, but the emails don't match
+      if @user != current_user
+        # associate
+      end
+
+      signed_in_root_path @user
     else
-      # Is the user logged in already? This will catch the case of me having a different email for FB acct than my current sevenzo account (at least, if i'm already signed in, otherwise, we won't know, and we'll create two accounts. Not sure that this edge case can be avoided.)
-        # "associate" their acc't with facebook
-        # Redirect to the homepage with flash message "You've successfully connected your Facebook account"
-      # Else,
-        # Assume they're a new user, and create an account
-      # End
+      # If not signed in, but the email is recognized
+      if @user
+        # If they're not associated with facebook
+        if @user.provider != 'facebook'
+          # associate
+        end
+
+      # If not signed in, and the email is not recognized
+      else
+        @user = User.from_omniauth(auth)
+      end
+
+      sign_in_and_redirect @user
     end
   end
 end
