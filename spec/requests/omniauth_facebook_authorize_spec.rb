bf51e53719c451d77e5d2861472b823b78fc7bc9
Karan Jain <kjain87@gmail.com>
2016-09-10 16:56:44 -0700

    added additional test cases

--- spec/requests/omniauth_facebook_authorize_spec.rb
+++ spec/requests/omniauth_facebook_authorize_spec.rb
@@ -3,15 +3,19 @@ require 'support/omniauth_helpers'
 
 RSpec.describe 'Facebook OAuth authorization', type: :request do
 
-  let(:oauth_strategy) { { strategy: 'facebook' } }
+  let(:provider) { 'facebook' }
+  let(:uid) { '12345' }
+  let(:email) { 'TEST@EXAMPLE.COM' }
+
+  let(:oauth_strategy) { { strategy: provider } }
   let(:valid_oauth_login) do
     oauth_strategy.merge(
       data: {
-        uid: '123545',
+        uid: uid,
         info: {
           first_name: 'Gaius',
           last_name:  'Baltar',
-          email:      'test@example.com'
+          email:      email,
         },
         credentials: {
           token: '123456',
@@ -42,8 +46,64 @@ RSpec.describe 'Facebook OAuth authorization', type: :request do
     context 'following redirect' do
       before { follow_redirect! }
 
+      it 'sets the attributes on the user' do
+        user = User.last
+        info = valid_oauth_login[:data][:info]
+        expect(user.first_name).to eq info[:first_name]
+        expect(user.last_name).to eq info[:last_name]
+        expect(user.email).to eq email.downcase
+        expect(user.avatar_option).to eq provider
+        expect(user.avatar.url).to include uid
+      end
+
       it 'redirects to complete profile path' do
         expect(response).to redirect_to edit_user_registration_path(setting: 'onboard')
+      end
+    end
+  end
+
+  context 'with valid login info for an existing user with same uid/provider' do
+    let!(:user) { create(:user, provider: provider, uid: uid) }
+    before { omniauth_authenticate(valid_oauth_login) }
+
+    it 'DOES NOT create a new user' do
+      expect(response).to be_redirect
+      expect { follow_redirect! }.not_to change(User, :count)
+    end
+
+    context 'following redirect' do
+      before { follow_redirect! }
+
+      it 'redirects to root path' do
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
+
+  context 'with valid login info for an existing complete user with same email' do
+    let!(:user) { create(:user, email: email) }
+    before { omniauth_authenticate(valid_oauth_login) }
+
+    it 'DOES NOT create a new user' do
+      expect(response).to be_redirect
+      expect { follow_redirect! }.not_to change(User, :count)
+    end
+
+    context 'following redirect' do
+      before { follow_redirect! }
+
+      it 'redirects to root path' do
+        expect(response).to redirect_to root_path
+      end
+
+      it 'updates their profile with oauth credentials but not name' do
+        user = User.last
+        info = valid_oauth_login[:data][:info]
+        expect(user.first_name).not_to eq info[:first_name]
+        expect(user.last_name).not_to eq info[:last_name]
+        expect(user.email).to eq email.downcase
+        expect(user.avatar_option).to eq provider
+        expect(user.avatar.url).to include uid
       end
     end
   end
