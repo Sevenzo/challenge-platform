129249b7e2e0f88d84250207e3c152e34ac74617
Stedman <stedmanhood@gmail.com>
2016-09-28 11:29:17 -0700

    Move sign in logic to the OmniauthCallbacksController, so that User#create_from_omniauth is only called in the case of a new user sign up. use define_method to dynamically define OmniauthCallbacksController#facebook and ...#twitter in a block, rather than a big, ugly %Q{...} string.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -5,57 +5,78 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       def #{provider}
         @user = User.find_for_oauth(env["omniauth.auth"], current_user)
 
-        if @user.persisted?
+        binding.pry
+        if @user.persisted? #even new users will be persisted at this point. Soooo need some other measure of whether they're "new" or not.
           sign_in_and_redirect @user, event: :authentication
           set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
         else
           session["devise.#{provider}_data"] = env["omniauth.auth"]
-          redirect_to new_user_registration_url
+          redirect_to new_user_registration_path
         end
+
+
       end
     }
   end
 
   [:twitter, :facebook].each do |provider|
-    provides_callback_for provider
-  end
+    define_method provider do
+      auth = request.env['omniauth.auth']
 
-  def after_sign_in_path_for(resource)
-    if resource.email_verified?
-      super resource
-    else
-      edit_user_registration_path(setting: 'onboard')
-    end
-  end
+      # =====================================
+      # TODO(Stedman): add #{provider} to path
+      return redirect_to user_facebook_omniauth_authorize(
+          auth_type: 'rerequest',
+          scope: 'email,public_profile'
+        ) if auth.info.email.blank?
 
-  # def facebook
-  #   auth = request.env['omniauth.auth']
+      # Find or create the identity with the given provider and uid.
+      @identity = Identity.find_or_create_from_omniauth(auth)
 
-  #   if auth.info.email.blank?
-  #     redirect_to user_facebook_omniauth_authorize(
-  #       auth_type: 'rerequest',
-  #       scope: 'email,public_profile'
-  #     )
-  #   else
-  #     user = User.find_by(provider: auth.provider, uid: auth.uid)
+      if signed_in?
+        if @identity.user == current_user
+          # User is signed in so they are trying to link an identity with their
+          # account. But we found the identity and the user associated with it
+          # is the current user. So the identity is already associated with
+          # this user. So let's display an error message.
+          redirect_to root_url, notice: "Already linked that account!"
+        else
+          # A currently signed in user always overrides the existing user
+          # to prevent the identity being locked with accidentally created accounts.
+          # Note that this may leave zombie accounts (with no associated identity) which
+          # can be cleaned up at a later date.
+          @identity.user = current_user
+          @identity.save
 
-  #     if user.nil?
-  #       user = User.find_by(email: auth.info.email.downcase)
+          # =====================================
+          # TODO(Stedman): Provider name:
+          redirect_to root_url, notice: "Successfully linked that account!"
+        end
+      else
+        @user = User.where(email: email).first
 
-  #       if user.nil?
-  #         user = User.create_from_omniauth(auth)
-  #       elsif user.uid != auth.uid
-  #         user.update_from_omniauth(auth)
-  #       end
-  #     end
+        if @identity.user.present?
+          # The identity we found had a user associated with it, so we log them in
+          sign_in_and_redirect @identity.user, event: :authentication
+        elsif @user
+          # A user with the provider's email address already exists.
+          # Associate the identity and log that user in.
+          @identity.user = @user
+          @identity.save
 
-  #     sign_in_and_redirect user
-  #   end
-  # end
+          sign_in_and_redirect @user, event: :authentication
+        else
+          # No user associated with this identity; treat this like a new user sign up.
+          @user = User.create_from_omniauth(env["omniauth.auth"])
+
+          sign_in_and_redirect @user
+        end
+      end
+    end # define_method provider
+  end # [:twitter, :facebook]
 
   def failure
     flash[:danger] = "Sorry, there was an error logging you in. Please try again, or contact us at <a href='mailto:#{ENV.fetch('APP_EMAIL')}' target='_blank'>#{ENV.fetch('APP_EMAIL')}</a> for further assistance.".html_safe
     redirect_to new_user_session_url
   end
-
 end
