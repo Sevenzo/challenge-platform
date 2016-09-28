adcbb0d6684430263f51efcbd0281e5c4895a492
Stedman <stedmanhood@gmail.com>
2016-09-27 23:34:11 -0700

    Add generic callback method for oauth providers. Configure omniauth for twitter in devise.rb.

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -5,4 +5,7 @@ class Identity < ActiveRecord::Base
   validates :uid,         presence: true, uniqueness: { scope: :provider }
   validates :provider,    presence:  true
 
+  def self.find_for_oauth(auth)
+    find_or_create_by(uid: auth.uid, provider: auth.provider)
+  end
 end
