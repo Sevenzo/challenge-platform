b4670d5838b891955dd2dff0898ecc0c5c7d0a8d
Karan Jain <kjain87@gmail.com>
2016-10-09 10:17:50 -0700

    Add Identity#find_or_create to persist all data (#70)

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -7,4 +7,10 @@ class Identity < ActiveRecord::Base
   validates :uid,       presence: true, uniqueness: { scope: :provider }
   validates :provider,  presence: true
 
+  def self.find_or_create_from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |identity|
+      identity.data = auth
+    end
+  end
+
 end
