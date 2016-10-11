39f050addc52ec0cc909b28e7824c2ca8b9bd9b0
Karan Jain <kjain87@gmail.com>
2016-10-10 20:36:50 -0700

    Added some comments to omniauthcallbacks controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -13,13 +13,13 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
       identity = Identity.find_or_initialize_from_omniauth(auth)
 
-      if user_signed_in?
+      if user_signed_in? # User is connecting from settings page; only can connect new services
 
-        if identity.new_record?
+        if identity.new_record? # should always be this case since they can't connect existing services
           identity.update!(user: current_user)
           current_user.send("update_from_#{provider}", auth)
           flash[:notice] = "Successfully linked your #{provider.capitalize} account!"
-        else
+        else # this means someone else has access to that account, do nothing
           flash[:error] = "Sorry, that #{provider.capitalize} account is registered with another user. If you think that is an error, please contact us at <%= ENV.fetch('APP_EMAIL') %>."
         end
 
@@ -28,17 +28,17 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       else
 
         user = identity.user
-        if user.nil?
+        if user.nil? # Identity is new record
           user = User.find_by(email: email)
 
-          if user.nil?
+          if user.nil? # No user with email, treat like new user signup
             user = User.send("create_from_#{provider}", auth)
             identity.update!(user: user)
           else
             existing_identity = user.send("#{provider}_identity")
-            if existing_identity
+            if existing_identity # User already has identity with that provider, simply update it
               existing_identity.update!(data: auth, uid: auth.uid)
-            else
+            else # User does not have identity with that provider, add to user
               identity.update!(user: user)
             end
             user.send("update_from_#{provider}", auth)
