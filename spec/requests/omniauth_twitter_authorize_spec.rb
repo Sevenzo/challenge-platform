3e286de80e3f4b43d8f198e32a11ad3bac000254
Stedman <stedmanhood@gmail.com>
2016-10-12 15:03:36 -0700

    Add full coverage to twitter login.

--- spec/requests/omniauth_twitter_authorize_spec.rb
+++ spec/requests/omniauth_twitter_authorize_spec.rb
@@ -76,5 +76,114 @@ RSpec.describe 'Twitter OAuth authorization', type: :request do
     end
   end
 
+  context 'with valid login info for an existing user with same uid/provider' do
+    let!(:user) { create(:user) }
+    let!(:identity) { create(:identity, provider: provider, uid: uid, user: user)}
+
+    before(:each) do
+      omniauth_authenticate(valid_oauth_login)
+    end
+
+    it 'redirects to callback' do
+      expect(response).to redirect_to user_twitter_omniauth_callback_path
+    end
+
+    context 'redirect to callback' do
+      before do
+        follow_redirect!
+      end
+
+      it 'redirects to root path' do
+        expect(response).to redirect_to root_path
+      end
+
+      it 'does not create a new user' do
+        # binding.pry
+        expect_any_instance_of(User.class).not_to receive(:update_from_omniauth)
+        expect { follow_redirect! }.not_to change(User, :count)
+      end
+
+      it 'displays the expected flash message' do
+        expect(flash[:notice]).to match('Successfully signed in with Twitter!')
+      end
+    end
+  end
+
+  context 'with valid login info for an existing user with same email' do
+    let!(:user) { create(:user, email: email) }
+    before { omniauth_authenticate(valid_oauth_login) }
+
+    it 'DOES NOT create a new user' do
+      expect(response).to be_redirect
+      expect { follow_redirect! }.not_to change(User, :count)
+    end
+
+    context 'following redirect' do
+      before { follow_redirect! }
+
+      it 'redirects to root path' do
+        expect(response).to redirect_to root_path
+      end
+
+      it 'displays the expected flash message' do
+        expect(flash[:notice]).to match('Successfully signed in with Twitter!')
+      end
+
+      it 'updates their profile with oauth credentials but not name' do
+        user = User.last
+        info = valid_oauth_login[:data][:info]
+        expect(user.first_name).not_to eq info[:first_name]
+        expect(user.last_name).not_to eq info[:last_name]
+        expect(user.location).to eq info[:location]
+        expect(user.avatar_option).to eq provider
+        expect(user.avatar.url).to include info[:nickname]
+      end
+    end
+  end
+
+  context 'with valid login info for an existing user with same email/uid' do
+    let!(:user) { create(:user, email: email) }
+    let!(:identity) { create(:identity, uid: uid, user: user)}
+
+    before { omniauth_authenticate(valid_oauth_login) }
+
+    context 'following redirect' do
+      before do
+        follow_redirect!
+      end
+
+      it 'redirects to root path' do
+        expect(response).to redirect_to root_path
+      end
+
+      it 'displays the expected flash message' do
+        expect(flash[:notice]).to match('Successfully signed in with Twitter!')
+      end
+
+      it 'does not create a new user' do
+        expect{ follow_redirect! }.not_to change(User, :count)
+      end
+    end
+  end
+
+  context 'with invalid login info for a new user' do
+    before { omniauth_authenticate(invalid_oauth_login) }
+
+    it 'redirects to callback' do
+      expect(response).to be_redirect
+    end
+
+    context 'following redirect' do
+      before { follow_redirect! }
+
+      it 'redirects to the failure callback' do
+        expect(response).to redirect_to '/users/auth/failure?message=invalid_credentials&strategy=twitter'
+      end
+    end
+
+    it 'does NOT create a new user' do
+      expect { follow_redirect! }.not_to change(User, :count)
+    end
+  end
 
 end
