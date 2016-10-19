316f8f9a69770c3bbab77827bb3404fe7d348b64
Stedman <stedmanhood@gmail.com>
2016-10-19 14:57:36 -0700

    Add descriptor below progress badges.

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -95,7 +95,7 @@ class Challenge < ActiveRecord::Base
       alias: 'story',
       action: 'share',
       icon: 'fa-paper-plane-o',
-      headline: 'Implement and Share Stories',
+      headline: 'Implement & Share',
       description: 'Try out and refine the bright spots with other peers and share your implementation stories!'
     }
   ]
