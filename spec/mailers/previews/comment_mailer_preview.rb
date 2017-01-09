9cf6d188fb9ead9b78cacc304690547d5cf5aaac
Karan Jain <kjain87@gmail.com>
2017-01-08 19:26:55 -0800

    LoM Breakout 8: Add templates for digest (#182)

--- spec/mailers/previews/comment_mailer_preview.rb
+++ spec/mailers/previews/comment_mailer_preview.rb
@@ -1,15 +1,35 @@
 class CommentMailerPreview < ActionMailer::Preview
-  
+
   def replied
-    CommentMailer.replied(Comment.where('parent_id IS NOT NULL').first.id)
+    comment = Comment.where('parent_id IS NOT NULL').first
+    user = comment.parent.user
+    CommentMailer.replied(comment.id, user.id)
   end
-  
+
   def posted
-    CommentMailer.posted(Comment.first.id)
+    comment = Comment.first
+    user =  comment.commentable.user
+    CommentMailer.posted(coment.id, user.id)
   end
 
   def followed
-    CommentMailer.followed(Comment.first.id, User.first.id)
+    comment = Comment.first
+    user =  User.first
+    CommentMailer.followed(comment.id, user.id)
+  end
+
+  def digest_daily
+    user = User.first
+    user.update!(digest_frequency: 'daily')
+    comment_ids = Notifications.scheduled_comments(user)
+    CommentMailer.digest(comment_ids, user.id)
+  end
+
+  def digest_weekly
+    user = User.first
+    user.update!(digest_frequency: 'weekly')
+    comment_ids = Notifications.scheduled_comments(user)
+    CommentMailer.digest(comment_ids, user.id)
   end
 
 end
