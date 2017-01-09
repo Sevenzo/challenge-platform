eeb48c4fa95ce23100aa604c52e14e92b72109b9
Karan Jain <kjain87@gmail.com>
2017-01-08 23:27:58 -0800

    Updated comment mailer queue method and properly configured mailcatcher (#186)
    * Updated comment mailer queue method and properly configured mailcatcher
    
    * Simplified digest email and clarified email subject lines
    
    * Updated footer unsubscribe copy

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -4,25 +4,25 @@ class CommentMailer < ApplicationMailer
   def replied(comment_id, user_id)
     @comment =  Comment.find(comment_id)
     @resource = User.find(user_id)
-    @subject = "#{@comment.user.display_name} replied to your comment on the #{@comment.commentable_type}: #{@comment.commentable_title.truncate(30)}"
+    @subject = "#{ENV.fetch('COMPANY_NAME')}: #{@comment.user.display_name} replied to your comment on #{@comment.commentable_title.truncate(30)}"
   end
 
   def posted(comment_id, user_id)
     @comment =  Comment.find(comment_id)
     @resource = User.find(user_id)
-    @subject = "#{@comment.user.display_name} commented on your #{@comment.commentable_type}: #{@comment.commentable_title.truncate(30)}"
+    @subject = "#{ENV.fetch('COMPANY_NAME')}: #{@comment.user.display_name} commented on #{@comment.commentable_title.truncate(30)}"
   end
 
   def followed(comment_id, user_id)
     @comment =  Comment.find(comment_id)
     @resource = User.find(user_id)
-    @subject = "#{@comment.user.display_name} added a comment in the discussion of #{@comment.commentable_type}: #{@comment.commentable_title.truncate(30)}"
+    @subject = "#{ENV.fetch('COMPANY_NAME')}: #{@comment.user.display_name} added a comment in the discussion of #{@comment.commentable_title.truncate(30)}"
   end
 
   def digest(comment_ids, user_id)
     @comments = comment_ids.transform_values { |ids| Comment.where(id: ids) unless ids.empty? }
     @resource = User.find(user_id)
-    @subject = "#{@resource.digest_frequency.capitalize} digest email for #{@resource.display_name}."
+    @subject = "#{ENV.fetch('COMPANY_NAME')}: #{@resource.digest_frequency.titleize} Activity Digest for #{@resource.display_name}"
   end
 
 end
